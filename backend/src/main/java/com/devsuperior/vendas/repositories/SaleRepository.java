9e2ce63fc3fd85b55673f092a84546ae4c6f7112
westorres9 <wester.torres9@gmail.com>
2022-07-28 20:37:23 -0300

    SaleRepository

---
+++ backend/src/main/java/com/devsuperior/vendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.vendas.repositories;
+
+import com.devsuperior.vendas.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
